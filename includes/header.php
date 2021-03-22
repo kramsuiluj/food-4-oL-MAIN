7b37ddf86e6091628521a27c11d9db341850df18
kramsuiluj <mjmaravillo@gmail.com>
2021-03-22 12:04:18 +0800

    added css styles link in header.php

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <style>
+        <?php require_once 'publice/styles.css'; ?>
+    </style>
     <title>food-4-oL</title>
 </head>
 <body>
