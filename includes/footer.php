90bcbd5fb8765768d549d9783fc5da443892e8f6
kramsuiluj <mjmaravillo@gmail.com>
2021-03-22 12:15:55 +0800

    added header, main content and footer in index.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <span>food-4-oL</span>
+        <p>Copyright © 2021</p>
+    </footer>
+    
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        <?php require_once 'publice/styles.css'; ?>
+        <?php require_once 'public/styles.css'; ?>
     </style>
     <title>food-4-oL</title>
 </head>
