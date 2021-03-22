c8c2b760d5febbad75167485b3fc3735a7a70bd6
kramsuiluj <mjmaravillo@gmail.com>
2021-03-22 12:03:02 +0800

    added header content in header.php file

---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>food-4-oL</title>
+</head>
+<body>
+
+    <header>
+
+        <div class="h-tl-container">
+            <span>
+                <a href="#">food-4-oL</a>
+            </span>
+        </div>
+
+        <div class="h-nav-container">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="h-btn-container">
+            <div>
+                <span>
+                    <a href="#" class="s-btn">Sign Up</a>
+                </span>
+                <span>
+                    <a href="#" class="l-btn">Login</a>
+                </span>
+            </div>
+        </div>
+
+    </header>
